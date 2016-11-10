c932fbc6b23b65813a62f36ce346303916dbbe95
Will Gorham <willgorham@gmail.com>
2016-11-10 15:37:00 -0500

    Set up for v0.2
    New data file
    Main nav wrapper

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
+
 /**
  * Main JSON object of posts, pages and settings
  */
@@ -8,45 +9,85 @@ var posts =
         "date":"2016-01-09T22:05:09",
         "modified":"2016-01-09T22:05:09",
         "slug":"hello-world",
-        "type":"posts",
+        "type":"post",
         "title":"Hello world!",
-        "content":"<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p> "
+        "content":"Welcome to WordPress. This is your first post. Edit or delete it, then start writing!"
       },
       {
         "id":2,
         "date":"2016-01-10T22:05:09",
         "modified":"2016-01-10T22:05:09",
         "slug":"learning-javascript",
-        "type":"posts",
+        "type":"post",
         "title":"Learning JavaScript!",
-        "content":"<p>I'm learning JavaScript and super excited!!!</p> "
+        "content":"I'm learning JavaScript and super excited!!!"
       },
       {
         "id":3,
         "date":"2016-01-11T22:05:09",
         "modified":"2016-01-11T22:05:09",
         "slug":"rest-api",
-        "type":"posts",
+        "type":"post",
         "title":"The REST API!",
-        "content":"<p>I've started working with the REST API in WordPress, what fun!</p> "
+        "content":"I've started working with the REST API in WordPress, what fun!"
       },
       {
         "id":4,
         "date":"2016-01-12T22:05:09",
         "modified":"2016-01-12T22:05:09",
         "slug":"json-data",
-        "type":"posts",
+        "type":"post",
         "title":"JSON Data!",
-        "content":"<p>So, with the REST API it is posible to pull in WordPress data as pure JSON.  Now I'm figuring out what to do with the data</p> "
+        "content":"So, with the REST API it is posible to pull in WordPress data as pure JSON.  Now I'm figuring out what to do with the data"
       },
       {
         "id":5,
         "date":"2016-01-13T22:05:09",
         "modified":"2016-01-13T22:05:09",
         "slug":"javascript-project",
-        "type":"posts",
+        "type":"post",
         "title":"JavaScript Project",
-        "content":"<p>I've started working with the REST API in WordPress, what fun!</p> "
+        "content":"I've started working with the REST API in WordPress, what fun!"
       }
     ],
-    jsonData = JSON.stringify( posts );
+    pages =
+    [
+      {
+        "id":6,
+        "date":"2016-01-18T22:05:09",
+        "modified":"2016-01-18T22:05:09",
+        "slug":"home",
+        "type":"page",
+        "title":"Home",
+        "content":"Welcome to VanillaPress, my JavaScript site!"
+      },
+      {
+        "id":7,
+        "date":"2016-01-18T22:05:09",
+        "modified":"2016-01-18T22:05:09",
+        "slug":"about",
+        "type":"page",
+        "title":"About",
+        "content":"A little about me!"
+      },
+      {
+        "id":8,
+        "date":"2016-01-18T22:05:09",
+        "modified":"2016-01-18T22:05:09",
+        "slug":"blog",
+        "type":"page",
+        "title":"Blog",
+        "content":"Please enjoy my posts"
+      },
+      {
+        "id":9,
+        "date":"2016-01-18T22:05:09",
+        "modified":"2016-01-18T22:05:09",
+        "slug":"contact",
+        "type":"page",
+        "title":"Contact",
+        "content":"Drop me a line with any questions :)"
+      }
+    ]
+    jsonData = JSON.stringify( { "posts": posts, "pages": pages } );
+    
